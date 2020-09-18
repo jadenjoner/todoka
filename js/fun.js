c39c8a409f8a794a88e19c2b9920efc9d3add8a0
Jaden <jadenjoner@gmail.com>
2020-09-18 12:55:09 -0700

    Added hide show func to query functions

--- js/fun.js
+++ js/fun.js
@@ -6,6 +6,13 @@ function $(query) {
       result.style[style] = styles[style];
     }
   };
+  result.hide = function(){
+    result.style.display = 'none';
+  }
+  result.show = function(display){
+    result.style.display = display;
+  }
+
 
   return result;
 }
@@ -20,6 +27,18 @@ function $$(query) {
       }
     }
   };
+  result.hide = function() {
+    result.forEach((c) => {
+      if(c.style != undefined)
+        c.style.display = "none";
+    })
+  }
+  result.show = function(display=""){
+    result.forEach((c) => {
+      if(c.style != undefined)
+        c.style.display = display;
+    })
+  }
 
   return result;
 }
